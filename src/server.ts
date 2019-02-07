246d24062fb93653358cd5ce8d2d2e77fc462adb
Jimmy Arroyo <jamesarroyo5@gmail.com>
2019-02-07 16:17:48 -0700

    Created a StarWarsAPI class with simple a simple structure supporting api routes '/people' and 'planets'.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import * as express from 'express';
+import { StarWarsAPI }  from './star-wars/star-wars';
 
 const port = 3000;
 const app = express();
+const starWarsRoutes = new StarWarsAPI().router;
+
+// Register routes
+app.use('/', starWarsRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
